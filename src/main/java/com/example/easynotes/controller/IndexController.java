b6858c8db1a7e9ed3b22e71165833beda7cc1646
John Davis <johndavisdigitalart@gmail.com>
2021-01-15 22:06:28 -0800

    Adding Thymeleaf

--- src/main/java/com/example/easynotes/controller/IndexController.java
+++ src/main/java/com/example/easynotes/controller/IndexController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
 
 //Controller
 //@RequestMapping("/")
@@ -19,4 +21,10 @@ public class IndexController {
         //return "Hello and Welcome to the EasyNotes application. You can create a new Note by making a POST request to /api/notes endpoint.";
         return "index";
     }
+
+    @GetMapping("/greeting")
+	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "greeting";
+	}
 }
