987987c87d83f167d8f8bd8d12acbb4f56b328fe
John Davis <johndavisdigitalart@gmail.com>
2021-01-15 21:46:14 -0800

    Getting some HTML content displaying

--- src/main/java/com/example/easynotes/controller/IndexController.java
+++ src/main/java/com/example/easynotes/controller/IndexController.java
@@ -4,13 +4,19 @@ package com.example.easynotes.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.stereotype.Controller;
 
-@RestController
-@RequestMapping("/")
+//Controller
+//@RequestMapping("/")
+@Controller
 public class IndexController {
 
-    @GetMapping
+    //@GetMapping
+    //@ResponseBody
+    @RequestMapping("/")
     public String sayHello() {
-        return "Hello and Welcome to the EasyNotes application. You can create a new Note by making a POST request to /api/notes endpoint.";
+        //return "Hello and Welcome to the EasyNotes application. You can create a new Note by making a POST request to /api/notes endpoint.";
+        return "index";
     }
 }
